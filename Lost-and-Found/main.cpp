91055e96130bd8dc65c7cdd44d73c7306d75afe9
ZingLix <ZingLix@users.noreply.github.com>
2018-11-21 18:51:25 +0800

    init commit

---
+++ Lost-and-Found/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "net/EventLoop.h"
+
+int main() {
+	EventLoop el;
+	return 0;
+}
---
+++ Lost-and-Found/net/EventLoop.h
@@ -0,0 +1,36 @@
+#ifndef LF_NET_EVENTLOOP
+#define LF_NET_EVENTLOOP
+
+#include <vector>
+#include <memory>
+#include <thread>
+
+class Channel;
+class Poller;
+
+class EventLoop
+{
+public:
+	EventLoop();
+	~EventLoop();
+
+	void loop();
+	void addChannel(Channel*);
+	void removeChannel();
+	static EventLoop* getCurrentThreadEventLoop();
+
+	void updateChannel(Channel* ch);
+	void removeChannel(Channel* ch);
+
+	static const int kPollTimeMs;
+
+private:
+	bool looping_;
+	bool quit_;
+	std::vector<std::shared_ptr<Channel>> channelList_;
+	std::unique_ptr<Poller> poller_;
+	const std::thread::id thread_id_;
+	std::vector<Channel*> activeList_;
+};
+
+#endif
